5be33d14f16ece84abeb38f6ec7b573e96fe9814
Affonso <affonso.neto@hotmail.com>
2015-05-19 18:41:36 +0000

    Cowsay scripty now works without any error. The last five tweets are show at start and a random cow is used every time.

--- twitter_monitor/CowTweet.py
+++ twitter_monitor/CowTweet.py
@@ -1,9 +1,17 @@
 import time
+import urllib3
+import certifi
+import logging
 from twython import TwythonStreamer
+from twython import Twython
 from subprocess import call
+import random
+
+logging.basicConfig(filename='test.log', level=logging.NOTSET)
+logging.captureWarnings(True)
 
 # Search terms
-TERMS = '#SpyPi'
+TERMS = '#SPyramide'
 
 #habe mich schon mal als Developer angemeldet, mit folgenden Token
 #Access Token 212884162-uaQigIHsBFbXWPBJCq8wKlriKfWMHmAlCTsIPPn7
@@ -16,8 +24,13 @@ OAUTH_TOKEN_SECRET = 'bS4rWtYjYUU83JuV9dR58seQBokukEyWVwnMNhGGTRqsj'
 
 tweets = []
 
+cows = [ 'default',
+         'vader',
+         'meow',
+         'turtle']
+
 def cowsay(tweet):
-        call(["/usr/games/cowsay", tweet])
+        call(["/usr/games/cowsay","-f", random.choice(cows) , tweet])
 
 # Setup callbacks from Twython Streamer
 class BlinkyStreamer(TwythonStreamer):
@@ -30,7 +43,14 @@ class BlinkyStreamer(TwythonStreamer):
 
 # Create streamer
 try:
-        cowsay("Tweete na #SPyramide")
+        cowsay("Tweeta na %s"%(TERMS))
+        stream = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        #stream.statuses.filter(track=TERMS)
+        tweets = stream.search(q=TERMS, count=5)
+        for t in tweets['statuses'][::-1]:
+                tweet = t['text'].encode('utf-8')
+                cowsay(tweet)
+                
         stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
         stream.statuses.filter(track=TERMS)
 except KeyboardInterrupt:
